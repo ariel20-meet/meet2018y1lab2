82bf3a891adb51ec77c7e45204bc27d5e627d64d
ariel20-meet <ariel20@meet.mit.edu>
2018-07-30 05:32:24 -0400

    Stopping here

---
+++ lab2.py
@@ -0,0 +1,73 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> a = 5
+>>> b = 10
+>>> a = b
+>>> b = a
+>>> a
+10
+>>> b
+10
+>>> a = 5
+>>> b = 10
+>>> c = 0
+>>> a = c
+>>> b = a
+>>> a = 5
+>>> b = 10
+>>> c = 0
+>>> a = 5
+>>> a = b
+>>> b = c
+>>> a
+10
+>>> b
+0
+>>> a = 5
+>>> b = 10
+>>> a
+5
+>>> b
+10
+>>> c
+0
+>>> c = a
+>>> a = b
+>>> b = c
+>>> a
+10
+>>> b
+5
+>>> four = '4'
+>>> print (four*3)
+444
+>>> five = 5
+>>> five = 4
+>>> print (five)
+4
+>>> print (five*3)
+12
+>>> ariel = 'student'
+>>> print ("hi," +myName')
+	   
+SyntaxError: EOL while scanning string literal
+>>> ariel=  = 'student'
+	   
+SyntaxError: invalid syntax
+>>> ariel = 'student'
+	   
+>>> 
+	   
+>>> print ("hi" = 'ariel')
+	   
+SyntaxError: keyword can't be an expression
+>>> my_name = 'student'
+	   
+>>> print ("hi" = my_name)
+	   
+SyntaxError: keyword can't be an expression
+>>> print ("hi" + my_name)
+	   
+histudent
+>>> 
